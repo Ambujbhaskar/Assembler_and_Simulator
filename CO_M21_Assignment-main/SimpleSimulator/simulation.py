749e37f180eed2b4ecb61cb6b9f450e60e982d11
Ambuj Bhaskar <89224125+Ambujbhaskar-iiitd@users.noreply.github.com>
2021-08-25 23:10:40 +0530

    final plot working correctly all tested

--- CO_M21_Assignment-main/SimpleSimulator/simulation.py
+++ CO_M21_Assignment-main/SimpleSimulator/simulation.py
@@ -17,7 +17,7 @@ def main():
     while not halted:
         inst = memory.fetch(PC.getVal(), cycle)     # it will fetch the instruction depending upon fetching PC value.
         halted, nextPC = executionEngine.execute(inst, cycle, PC.PC)    # it will execute instruction and update the register_file.
-        PClst.append(PC.getVal)        # for plot
+        PClst.append(PC.getPCdec())        # for plot
         cyclelst.append(cycle+1)    # for plot
         pc_print = PC.dump()
         reg_print = registerFile.dump()
