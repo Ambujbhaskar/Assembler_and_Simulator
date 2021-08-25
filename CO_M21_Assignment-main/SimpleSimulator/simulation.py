33d2bbdea6a0497e8b14265267140b3b2cc6f06a
Ambuj Bhaskar <89224125+Ambujbhaskar-iiitd@users.noreply.github.com>
2021-08-25 22:45:24 +0530

    Corrected plot - final

--- CO_M21_Assignment-main/SimpleSimulator/simulation.py
+++ CO_M21_Assignment-main/SimpleSimulator/simulation.py
@@ -11,13 +11,13 @@ def main():
     executionEngine = executionengine(memory, registerFile)     # execution engine change memory and registerfile depending upon what ever instruction are given.
     PC = programcounter(0)
     halted = False
-    cycle = 0
+    cycle = 1
     PClst = []
     cyclelst = []
     while not halted:
         inst = memory.fetch(PC.getVal(), cycle)     # it will fetch the instruction depending upon fetching PC value.
         halted, nextPC = executionEngine.execute(inst, cycle, PC.PC)    # it will execute instruction and update the register_file.
-        PClst.append(nextPC)        # for plot
+        PClst.append(getVal)        # for plot
         cyclelst.append(cycle+1)    # for plot
         pc_print = PC.dump()
         reg_print = registerFile.dump()
